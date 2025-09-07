38783aa66d5087d351bcf13ebdbb2b925f919f3c
AZIZ ULLAH TAREK <azizullaht2002@gmail.com>
2025-09-08 01:19:09 +0600

    some javascript start

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("js connetced");
+const categoryContainer = document.getElementById("categoryContainer");
+const cardContainer = document.getElementById("cardContainer");
+const callBtn = document.getElementById("call-btn");
+
+let cart = [];
+
+// Load all categories
+const loadCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then((data) => {
+      const categories = data.categories;
+      showCategory(categories);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// Show categories
+const showCategory = (categories) => {
+  // Add All Plants first
+  categoryContainer.innerHTML = `
+    <li id="all" class="space-y-6 pt-3 text-[16px] bg-green-600 rounded-sm p-1 cursor-pointer">
+      All Plants 
+    </li>
+  `};
